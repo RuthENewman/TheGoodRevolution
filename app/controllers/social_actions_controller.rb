b335666d42096daf934e664db74a28f0d23bf9e8
github username <github email address>
2019-01-22 15:14:59 +0000

    latest version working on new social action form

--- app/controllers/social_actions_controller.rb
+++ app/controllers/social_actions_controller.rb
@@ -1,5 +1,5 @@
 class SocialActionsController < ApplicationController
-  before_action :set_social_action, only: [:show]
+  before_action :set_social_action, only: [:show, :edit, :destroy]
 
 
   def index
@@ -9,10 +9,45 @@ class SocialActionsController < ApplicationController
   def show
   end
 
+  def new
+    @social_action = SocialAction.new
+  end
+
+  def create
+    @social_action = SocialAction.new(social_action_params)
+    if @social_action.valid?
+      @social_action.save
+      redirect_to @social_action
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @social_action.update(social_action_params)
+      if @social_action.valid?
+        redirect_to @social_action
+      else
+        render 'new'
+      end
+    end
+
+  def destroy
+    @social_action.destroy
+    redirect_to social_actions_path
+  end
+
   private
 
   def set_social_action
     @social_action = SocialAction.find(params[:id])
+  end
+
+  def social_action_params
+    params.require(:social_action).permit(:title, :charity_id, :summary, :img_url)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'welcome/index', to: 'welcome#index' #, as 'welcome'
+  get '/', to: 'welcome#index', as: 'welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/charities', to: 'charities#index'
-  resources :charities
-  resources :social_actions
-  resources :events
-  root 'welcome#index'
-  # get '/social_actions', to:'social_actions#index'
-  # get '/events', to:'events#index'
-  # get '/charities/:id', to: 'charities#show'
-  # get '/social_actions/:id' , to: 'social_actions#show'
-  # get '/events/:id', to: 'events#show'
+  get '/charities', to: 'charities#index'
+  # resources :charities
+  # resources :social_actions
+  # resources :events
+  # root 'welcome#index'
+  get '/social_actions', to:'social_actions#index', as: 'social_actions'
+  get '/events', to:'events#index', as: 'events'
+  get '/charities/new', to: 'charities#new', as: 'new_charity'
+  get '/charities/:id', to: 'charities#show', as: 'charity'
+  get '/social_actions/new', to: 'social_actions#new', as: 'new_social_action'
+  get '/social_actions/:id' , to: 'social_actions#show', as: 'social_action'
+  get '/events/:id', to: 'events#show', as: 'event'
 end
